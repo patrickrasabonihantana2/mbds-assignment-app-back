a6a70f8f9cce9094cab2a00e58594174daaa03cd
Patrick <patrickrasabonihantana2@gmail.com>
2024-05-21 23:05:16 +0300

    configure dev env

--- src/util/env.js
+++ src/util/env.js
@@ -6,14 +6,14 @@ dotenvExpand.expand(env);
 
 const Env = {
     MONGO_URL: process.env.MONGO_URL || 'mongodb://root:example@mongo:27017/',
-    MONGO_DB: process.env.MONGO_DB || 'garage_test',
+    MONGO_DB: process.env.MONGO_DB || 'assignment_db',
     SECURITY_CORS_ALLOW_ORIGIN: process.env.SECURITY_CORS_ALLOW_ORIGIN || "",
     SECURITY_CORS_ALLOW_METHODS: process.env.SECURITY_CORS_ALLOW_METHODS || "",
     SECURITY_CORS_ALLOW_HEADERS: process.env.SECURITY_CORS_ALLOW_HEADERS || "",
     SECURITY_CORS_ALLOW_CREDENTIALS:
       (process.env.SECURITY_CORS_ALLOW_CREDENTIALS || "true").toLowerCase() ===
       "true",
-    SECURITY_JWT_SECRET: process.env.SECURITY_JWT_SECRET || '3436864e-f3ef-4fc7-8c82-6fb0c08ea697'
+    SECURITY_JWT_SECRET: process.env.SECURITY_JWT_SECRET || 'd5951f0a-25b6-45f4-9b91-70a6b1fca0b0'
 };
 
 module.exports = Env;
