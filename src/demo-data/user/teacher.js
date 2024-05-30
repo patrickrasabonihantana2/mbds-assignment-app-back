29c8d577b047e646dea69ed6df68aa6aa146f899
Patrick <patrickrasabonihantana2@gmail.com>
2024-05-30 23:53:14 +0300

    demo data

---
+++ src/demo-data/user/teacher.js
@@ -0,0 +1,30 @@
+const teachers = [
+  {
+    user: {
+      number: '000002',
+      firstname: 'Michel',
+      lastname: 'Buffa',
+      email: 'admin@miage.com',
+      role: 'TEACHER'
+    },
+    userLogin: {
+      username: 'michel',
+      password: 'michel'
+    }
+  },
+  {
+    user: {
+      number: '000003',
+      firstname: 'Gabriel',
+      lastname: 'Mopolo',
+      email: 'admin@miage.com',
+      role: 'TEACHER'
+    },
+    userLogin: {
+      username: 'gabriel',
+      password: 'gabriel'
+    }
+  }
+];
+
+module.exports = teachers;
